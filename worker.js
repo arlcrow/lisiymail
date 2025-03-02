66e77031e59274d0b58a77192e4a57a636c25716
Matvey Aleksandrov <matvey@arlcrow.site>
2025-03-02 20:04:07 +0300

    initial commit

---
+++ worker.js
@@ -0,0 +1,16 @@
+export default {
+  async email(message, env, ctx) {
+    const blockList = ["hacker@example.com", "spammer@example.com"]
+    if (blockList.includes(message.from)) {
+      message.setReject("Address is blocked");
+      return;
+    }
+    await message.forward("inbox@corp");
+  },
+  async fetch(request, env, ctx) {
+		const url = new URL(request.url);
+		console.log(`Hello ${navigator.userAgent} at path ${url.pathname}!`);
+
+		return new Response.json({"hello": "world"});
+	}
+}
